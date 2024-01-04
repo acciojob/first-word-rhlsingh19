40d674ebb60cee411345975afc9b157c2483f3e2
rhlsingh19 <145037560+rhlsingh19@users.noreply.github.com>
2024-01-04 21:21:48 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-function firstWord(s) {
-  // your code here
+function firstWord(str) {
+    if (!str || str.indexOf(' ') === -1) {
+        return str;
+    }
+
+    const spaceIndex = str.indexOf(' ');
+    return str.substring(0, spaceIndex);
 }
+
+// Examples
+console.log(firstWord('see and stop')); // Output: 'see'
+console.log(firstWord(' Hello World!')); // Output: 'Hello'
+console.log(firstWord('12345')); // Output: '12345'
+console.log(firstWord('')); // Output: ''
+
+
 
 // Do not change the code below
 
